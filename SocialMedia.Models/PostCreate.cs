615f73d9cea5eed22f5af85a56372464288ee3f3
Seth-Thomas <Seth.b.thomas8@gmail.com>
2021-02-05 16:15:59 -0500

    Update

---
+++ SocialMedia.Models/PostCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter at least 1 character.")]
+        [MaxLength(150, ErrorMessage = "Only 150 characters allowed in your post.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Text { get; set; }
+        //public Guid Author { get; set; }
+    }
+}
---
+++ SocialMedia.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,70 @@
+﻿using SocialMedia.Models;
+using SocialMedia.Data;
+using SocialMedia.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia.WebAPI.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPost();
+            return Ok(post);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private PostService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+        public IHttpActionResult Get(int id)
+        {
+            PostService noteService = CreatePostService();
+            var note = noteService.GetPostById(id);
+            return Ok(note);
+        }
+        public IHttpActionResult Put(PostEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdatePost(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+
+            if (!service.DeletePost(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+        
+}
