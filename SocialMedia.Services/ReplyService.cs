789178c82c9353788f474048c36f7f1e1dc68c89
MaryCaroleScannnell <73565044+MaryCaroleScannnell@users.noreply.github.com>
2021-02-04 21:34:43 -0500

    MC thursday push

---
+++ SocialMedia.Services/ReplyService.cs
@@ -0,0 +1,57 @@
+﻿using SocialMedia.Data;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+    public class ReplyService
+    {
+        private readonly Guid _authorId;
+
+        public ReplyService(Guid AuthorId)
+        {
+            _authorId = AuthorId;
+        }
+
+        public bool CreateReply(ReplyCreate model)
+        {
+            var entity =
+                new Reply()
+                {
+                    Author = _authorId,
+                    Text = model.Text,
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Replies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<ReplyListItem> GetReply()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Replies
+                        .Where(e => e.Author == _authorId)
+                        .Select(
+                            e =>
+                                new ReplyListItem
+                                {
+                                    Id = e.Id,
+                                    Text = e.Text,
+                                    
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
