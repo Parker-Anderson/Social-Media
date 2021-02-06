09185346bef3713bb468cd9bc998b9304fff62ab
Parker Anderson <parker.anderson0810@outlook.com>
2021-02-06 11:03:59 -0500

    added CommentDbContext

---
+++ SocialMedia.Models/CommentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class CommentDbContext: DbContext
+    {
+        public CommentDbContext()
+            : base("DefaultConnection") { }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
