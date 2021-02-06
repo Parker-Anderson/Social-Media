25cfa9906e0a1e99244842c6c3f24be34080776c
MaryCaroleScannnell <73565044+MaryCaroleScannnell@users.noreply.github.com>
2021-02-06 11:36:23 -0500

    update from MC

--- SocialMedia.Data/Reply.cs
+++ SocialMedia.Data/Reply.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Data
 {
-   public class Reply
+    public class Reply
     {
         [Key]
         public int Id { get; set; }
+        
+
+        [ForeignKey(nameof(Replies))]
+        public int ReplyId { get; set; }
+        
+        //navigation property
+        public virtual List<Reply> Replies { get; set; } = new List<Reply>();
+
         [Required]
         public string Text { get; set; }
         [Required]
